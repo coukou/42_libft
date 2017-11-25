019c8fdaaf6701ff9521edf60e53a59adb663a95
= <orenkay57@gmail.com>
2017-11-25 01:12:22 +0100

    si j'oublie les casts c'est débile

--- srcs/ft_strjoin_free.c
+++ srcs/ft_strjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: spopieul <spopieul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/25 00:59:57 by spopieul          #+#    #+#             */
-/*   Updated: 2017/11/25 01:06:31 by spopieul         ###   ########.fr       */
+/*   Updated: 2017/11/25 01:11:42 by spopieul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strjoin_free(const char *s1, const char *s2)
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	out = ft_strjoin(s1, s2);
-	ft_strdel(&s1);
-	ft_strdel(&s2);
+	ft_strdel((char**)&s1);
+	ft_strdel((char**)&s2);
 	return (out);
 }
